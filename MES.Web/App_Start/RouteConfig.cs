a8ab693d87e91b6eb5f0fd2b26a593716ab2225b
wangyh <wangyh@hanshuntech.com>
2017-06-04 22:54:59 +0800

    222

--- MES.Web/App_Start/RouteConfig.cs
+++ MES.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace MES.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Admin",
+                url: "app/admin/{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "app/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- MES.Web/Models/UserModels.cs
+++ MES.Web/Models/UserModels.cs
@@ -42,10 +42,43 @@ namespace MES.Web.Models
     {
         [Key]
         [Column("OPERID")]
+        [Display(Name="用户名")]
+        [Required, MaxLength(20)]
         public String ID { get; set; }
+
+        [Display(Name = "姓名")]
         [Column("OPERNAME")]
+        [Required, MaxLength(80)]
         public String Name{ get; set; }
+
+        [Display(Name="密码")]
         [Column("PWD")]
+        [Required, MaxLength(128)]
         public String Passwd{ get; set; }
+
+        [Display(Name = "部门代码")]
+        [Column("DEPTNO")]
+        [Required, MaxLength(20)]
+        public String DeptNo { get; set; }
+
+        [Display(Name = "公司代码")]
+        [Column("BUNO")]
+        [Required, MaxLength(20)]
+        public String BuNo { get; set; }
+
+        [Display(Name = "Email")]
+        [Column("Email")]
+        [MaxLength(120)]
+        public String Email { get; set; }
+
+        [Display(Name = "手机号")]
+        [Column("MOBILE")]
+        [MaxLength(80)]
+        public String Mobile { get; set; }
+
+        [Display(Name = "备注")]
+        [Column("COMMENTS")]
+        [MaxLength(250)]
+        public String Comments { get; set; }
     }
 }
